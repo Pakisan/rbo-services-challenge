ffbe887254e6cb683688f9f693198e1b0292677f
Pavel Bodyachevskiy <pavelbodyachevskiy@gmail.com>
2022-10-03 14:50:32 +0400

    initial commit

---
+++ src/main/java/com/gihub/pakisan/pubsub/RboServicesApplication.java
@@ -0,0 +1,13 @@
+package com.gihub.pakisan.pubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RboServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RboServicesApplication.class, args);
+    }
+
+}
